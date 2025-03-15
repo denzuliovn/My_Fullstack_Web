31c3991ab67907a302fa2b50644d6857e4004971
Tran Manh Duc <117091219+dustinvn@users.noreply.github.com>
2025-03-15 02:07:00 +0000

    cap nhat login

--- test-project/server/index.js
+++ test-project/server/index.js
@@ -1,36 +1,48 @@
-import { createServer } from 'node:http'
-import { createYoga } from 'graphql-yoga'
-import { schema } from './graphql/schema.js'
-import { useGraphQLMiddleware } from '@envelop/graphql-middleware'
-import { permissions } from './permissions.js'
-import { db } from './config.js'
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
+import { schema } from "./graphql/schema.js";
+import { useGraphQLMiddleware } from "@envelop/graphql-middleware";
+import { permissions } from "./permissions.js";
+import { db } from "./config.js";
 
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import { initDatabase } from './data/init.js'
-await initDatabase()
+import { initDatabase } from "./data/init.js";
+await initDatabase();
+
+const signingKey = process.env.JWT_SECRET;
+import jwt from "jsonwebtoken";
 
 const yoga = createYoga({
   schema,
-  graphqlEndpoint: '/',
+  graphqlEndpoint: "/",
   plugins: [useGraphQLMiddleware([permissions])],
   context: async ({ request }) => {
+    const authorization = request.headers.get("authorization") ?? "";
+
+    if (authorization.startsWith("Bearer")) {
+      const token = authorization.substring(7, authorization.length);
+      jwt.verify(token, signingKey, function (error, decoded) {
+        let user = null;
+        if (!error) {
+          user = decoded;
+        }
+
+        return {
+          db: db,
+          user: user,
+        };
+      });
+    }
     return {
       db: db,
-      secret: request.headers.get('secret') ?? '',
-    }
+    };
   },
-  // cors: {
-  //   origin: '*',
-  //   credentials: true,
-  //   allowedHeaders: ['X-Custom-Header', 'content-type'],
-  //   methods: ['POST'],
-  // },
-})
-const server = createServer(yoga)
+});
+const server = createServer(yoga);
 
-const PORT = 4000
+const PORT = 4000; // process.env.PORT
 server.listen(PORT, () => {
-  console.info(`Server is running on http://localhost:${PORT}`)
-})
+  console.info(`Server is running on http://localhost:${PORT}`);
+});
